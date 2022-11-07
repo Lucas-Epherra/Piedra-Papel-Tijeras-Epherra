3ccbfefb005e97dc09335e3eaf1ae07f2093a207
Lucas Epherra <letoso13@gmail.com>
2022-11-07 11:13:32 -0300

    juego casi finalizado

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // PIEDRA PAPEL O TIJERA
 
 //alert de bienvenida al usuario
-alert("Bienvenido a piedra papel o tijeras, pulse aceptar para continuar");
+const bienvenida = () => {
+    alert("Bienvenido a piedra papel o tijeras, pulse aceptar para continuar");
+}
+
+bienvenida();
 
 let usuario;
+let PC = "computadora";
 
 // funcion que captura la entrada y le da valor a la variable usuario
 const entradaUsuario = () => {
@@ -13,20 +18,42 @@ const entradaUsuario = () => {
 
 usuario = entradaUsuario();
 
-console.log(usuario);
+console.log("El jugador es " + usuario);
+
+
+//objetos de jugadores con nombre y puntuacion
+
+let puntos = [
+    jugador = {
+        nombre: usuario,
+        puntaje: 0,
+    },
+    jugadorPc = {
+        nombre: PC,
+        puntaje: 0,
+    }
+]
 
 //array de armas
-const armas = ["piedra","papel","tijera"]
+const armas = ["piedra", "papel", "tijera"]
 
 //funcion para que el ordenador elija su arma
-function elegirArma (arr) {
-    return arr [Math.floor(Math.random() * arr.length )];
+function elegirArma(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
 }
 
 // la variable toma el valor del resultado de la funcion de arriba
-let armaCpu = elegirArma(armas);
+let armaCpu;
+
+//funcion flecha para mostrar el menu de opciones
+const opciones = () => {
+    return ("Hola " + usuario + " !! ingresa tu eleccion: \nOpciones: \n1 = Piedra \n2 = Papel \n3 = Tijera")
+}
 
 //funcion para validar el numero ingresado , permitiendo solo de 1 al 3
+let cortarFuncion = true;
+let opcionJugador;
+
 function validarNumero() {
     while (cortarFuncion == true) {
         opcionJugador = parseInt(prompt(opciones()));
@@ -40,52 +67,95 @@ function validarNumero() {
     }
 }
 
+let ganador;
+
 //Funcion para la batalla
 const batalla = () => {
     if (opcionJugador == "piedra" && armaCpu == "tijera") {
+        ganador = usuario;
         alert("Has ganado :D !!");
-        console.log("El usuario " + usuario + " ha ganado");
+        console.log("El jugador " + usuario + " ha ganado");
     } else if (opcionJugador == "papel" && armaCpu == "piedra") {
+        ganador = usuario;
         alert("Has ganado :D !!");
-        console.log("El usuario " + usuario + " ha ganado");
+        console.log("El jugador " + usuario + " ha ganado");
     } else if (opcionJugador == "tijera" && armaCpu == "papel") {
+        ganador = usuario;
         alert("Has ganado :D !!");
-        console.log("El usuario" + usuario + " ha ganado");
+        console.log("El jugador" + usuario + " ha ganado");
     } else if (opcionJugador == armaCpu) {
+        ganador = "empate";
         alert("Es un empate :O !!");
         console.log("Ha ocurrido un empate");
     } else {
+        ganador = PC;
         alert("Has perdido :C !!");
-        console.log("El usuario " + usuario + " ha perdido");
+        console.log("El jugador " + usuario + " ha perdido");
     }
 }
 
-//funcion flecha para mostrar el menu de opciones
-const opciones = () => {
-    return ("Hola " + usuario + " !! ingresa tu eleccion: \nOpciones: \n1 = Piedra \n2 = Papel \n3 = Tijera")
+//funcion con switch para transformar el numero a string con el valor deseado
+const transformToString = () => {
+    switch (opcionJugador) {
+        case 1:
+            opcionJugador = "piedra"
+            break;
+        case 2:
+            opcionJugador = "papel"
+            break;
+        case 3:
+            opcionJugador = "tijera"
+            break;
+    }
 }
 
-let cortarFuncion = true;
-let opcionJugador;
 
-opcionJugador = validarNumero();
-
-// switch para transformar el numero a string con el valor deseado
-switch (opcionJugador) {
-    case 1:
-        opcionJugador = "piedra"
-        break;
-    case 2:
-        opcionJugador = "papel"
-        break;
-    case 3:
-        opcionJugador = "tijera"
-        break;
+const scores = () => {
+    if (ganador === usuario) {
+        puntos[0].puntaje++;
+        console.log("el jugador " + usuario + " tiene " + puntos[0].puntaje + " puntos")
+    } else if (ganador === PC) {
+        puntos[1].puntaje++;
+        console.log("el jugador pc tiene " + puntos[1].puntaje + " puntos")
+    } else {
+        console.log("empate no mueve puntos")
+        console.log("el jugador " + usuario + " tiene " + puntos[0].puntaje + " puntos")
+        console.log("el jugador pc tiene " + puntos[1].puntaje + " puntos")
+    }
 }
 
-console.log("El usuario ha elegido " + opcionJugador);
+// funcion que llama a las funciones declaradas anteriormente para ejecutar el juego
 
-console.log("La computadora ha elegido " + armaCpu);
+// let score;
 
-batalla();
+const ejecutarJuego = () => {
 
+    armaCpu = elegirArma(armas);
+
+    opcionJugador = validarNumero();
+
+    transformToString();
+
+    batalla();
+
+    console.log("El jugador " + usuario + " ha elegido " + opcionJugador);
+
+    console.log("La computadora ha elegido " + armaCpu);
+        
+    scores();
+}
+
+let pararJuego = true;
+let campeon;
+
+const juegoDeCincoRondas = () => {
+    while (pararJuego == true) {
+        ejecutarJuego();
+        if (puntos[0].puntaje < 5 || puntos[1].puntaje < 5) {
+            pararJuego == false;
+            return campeon;
+        }
+    }
+}
+
+campeon = juegoDeCincoRondas();
